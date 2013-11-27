092804cf8c1ea742db8441428990b3fac891c68a
Niclas Olofsson <n@niclasolofsson.se>
2013-11-27 15:15:54 +0100

    Sequential mergesort

--- lab3/sort.c
+++ lab3/sort.c
@@ -24,6 +24,7 @@
 // Do not touch or move these lines
 #include <stdio.h>
 #include "disable.h"
+#include <stdlib.h>
 
 #ifndef DEBUG
 #define NDEBUG
@@ -33,11 +34,44 @@
 #include "sort.h"
 #include "simple_quicksort.h"
 
+int sequentialMergesort(value*, int);
+void sequentialMerge(value*, int, int);
+
 int
 sort(struct array * array)
 {
-	simple_quicksort_ascending(array);
+	//simple_quicksort_ascending(array);
 
+	sequentialMergesort(array->data, array->length);
 	return 0;
 }
 
+
+int sequentialMergesort(value *array, int n) {
+	if(n==1) {
+		return 0;
+	}
+
+	int middle = n/2;
+	sequentialMergesort(array, middle);
+	sequentialMergesort(array + middle, n - middle);
+
+	sequentialMerge(array, middle, n);
+	return 0;
+}
+
+void sequentialMerge(value *array, int middle, int n) {
+	int i0 = 0, i1 = middle, j;
+	int* temp = malloc(n*sizeof(value));
+
+	for (j = 0; j < n; j++) {
+		if (i0 < middle && (i1 >= n || array[i0] <= array[i1])) {
+			temp[j] = array[i0++];
+		} else {
+			temp[j] = array[i1++];
+		}
+	}
+	for (j = 0; j < n; j++) {
+		array[j] = temp[j];
+	}
+}
